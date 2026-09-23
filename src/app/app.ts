c0784ca4996d008abc359f736988faeae12110ad
bagouser123 <lazarev.illia.2017@gmail.com>
2026-09-23 19:36:35 +0300

    pushing working files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MovieService } from './services/movie';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('super-giper-duper-new-repository');
+
+  title2 = '';
+  year: number | null = null;
+  rating: number | null = null;
+
+  constructor(private movieService: MovieService) {}
+
+  get movies() {
+    return this.movieService.getMovies();
+  }
+
+  onAddMovie() {
+    if (!this.title2 || this.year === null || this.rating === null) {
+      return;
+    }
+    this.movieService.addMovie(this.title2, this.year, this.rating);
+    this.title2 = '';
+    this.year = null;
+    this.rating = null;
+  }
+
+  onRemoveMovie(id: number) {
+    this.movieService.removeMovie(id);
+  }
 }
--- src/app/services/movie.ts
+++ src/app/services/movie.ts
@@ -1,4 +1,37 @@
-import { Service } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
-@Service()
-export class Movie {}
+export interface Movie {
+  id: number;
+  title: string;
+  year: number;
+  rating: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  private movies = signal<Movie[]>([
+    { id: 1, title: 'Interstellar', year: 2014, rating: 9 },
+    { id: 2, title: 'Avatar', year: 2009, rating: 8 },
+    { id: 3, title: 'Titanic', year: 1997, rating: 7 }
+  ]);
+
+  getMovies() {
+    return this.movies;
+  }
+
+  addMovie(title: string, year: number, rating: number) {
+    const newMovie: Movie = {
+      id: Date.now(),
+      title,
+      year,
+      rating
+    };
+    this.movies.update(movies => [...movies, newMovie]);
+  }
+
+  removeMovie(id: number) {
+    this.movies.update(movies => movies.filter(movie => movie.id !== id));
+  }
+}
